d027a739fd587736d0e97e08e5b81a3fba2ca6bd
Moutasem-Ahmed-Salah <moutasemahmed122@gmail.com>
2024-10-27 02:08:23 +0300

    Conditional rendering the cart count

--- src/App.js
+++ src/App.js
@@ -131,9 +131,11 @@ function App() {
                 data-testid="cart-btn"
                 onClick={() => setShowModal(!showModal)}
               />
-              <span className="absolute -top-2 -right-2 bg-red-600 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
-                {cartCount}
-              </span>
+              {cartCount != 0 && (
+                <span className="absolute -top-2 -right-5 bg-black text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
+                  {cartCount}
+                </span>
+              )}
             </li>
           </ul>
           <div className="  z-50">
